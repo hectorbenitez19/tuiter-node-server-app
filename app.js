b9be09d3665b24601882a9b5d7c2f9265d2e9e73
hectorbenitez19 <beniez.h@husky.neu.edu>
2023-04-03 00:25:01 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+app.use(cors());
+import HelloController
+  from "./controllers/hello-controller.js";
+import UserController
+  from "./controllers/users/users-controller.js";
+
+  import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+
+app.use(express.json());
+TuitsController(app);
+HelloController(app);
+UserController(app);
+app.listen(process.env.PORT || 4000);
