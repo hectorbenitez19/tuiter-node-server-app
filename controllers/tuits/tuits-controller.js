e5d2daa70b318dd30ec2721c8c6c4ba5e49c3fc5
hectorbenitez19 <beniez.h@husky.neu.edu>
2023-04-10 14:23:28 -0400

    updates

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -3,10 +3,10 @@ import * as tuitsDao from './tuits-dao.js';
 
 const createTuit = async (req, res) => {
   const newTuit = req.body;
- // newTuit._id = (new Date()).getTime()+'';
+
   newTuit.likes = 0;
   newTuit.liked = false;
-  //tuits.push(newTuit);
+
   const insertedTuit = await tuitsDao.createTuit(newTuit);
   res.json(insertedTuit);
 }
@@ -30,8 +30,7 @@ const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
     const status = await tuitsDao
                          .deleteTuit(tuitdIdToDelete);
- // tuits = tuits.filter((t) =>
-   // t._id !== tuitdIdToDelete);
+
   res.json(status);
 }
 
