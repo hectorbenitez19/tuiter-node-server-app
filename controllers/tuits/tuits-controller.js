1702b1abea1d2f28fd86e0050f1e4655f59d7a62
hectorbenitez19 <beniez.h@husky.neu.edu>
2023-04-10 11:29:49 -0400

    update node project

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,6 +1,6 @@
 import posts from "./tuits.js";
 let tuits = posts;
-import * as tuitsDao from '../../tuits/tuits-dao.js';
+import * as tuitsDao from './tuits-dao.js';
 
 const createTuit = async (req, res) => {
   const newTuit = req.body;
@@ -12,9 +12,10 @@ const createTuit = async (req, res) => {
   res.json(insertedTuit);
 }
 
-const findTuits = async (req, res) =>
-    tuits = await tuitsDao.findTuits();
+const findTuits = async (req, res) => {
+   const tuits = await tuitsDao.findTuits();
    res.json(tuits);
+   }
 
 const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
